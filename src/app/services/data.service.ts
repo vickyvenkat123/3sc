e494bd5955a27f7a67617d100f5c2b475d63bf43
Venkatraman P <Venkatraman.p@3SCSOLUTION.LAN>
2021-08-31 18:17:37 +0530

    3sc

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -8,9 +8,6 @@ export class DataService {
 
   constructor(public http : HttpClient) { }
   getTableData(){
-    var token = sessionStorage.getItem("token") || "";
-    const headers = new HttpHeaders().set('Authorization', token);
-    headers.append("Content-Type", "application/json");
     return this.http.get('../assets/json/tableData.json');
   }
 }
